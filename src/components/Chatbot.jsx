ca8220b0ef2c1cd3ecb625686bc6201402d1f741
Katya Vázquez <116518901+KatyaVazquez@users.noreply.github.com>
2024-07-20 11:47:48 -0400

    feat: connect wit map

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -6,9 +6,18 @@ const openai = new OpenAI({
   dangerouslyAllowBrowser: true
 });
 
-async function main() {
+async function getData(userMessage) {
     const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: 'Hola' }],
+      messages: [{ role: 'system', content: `el usuario te pedira lugares turisticos, debes responder con lugares turisticos de ese lugar, pero en formato json, con este formato, no respondas de otra manera: {
+        "key": "Nombre del lugar",
+        "type": "El tipo del lugar. Catgorias: Turístico, Comida, Desayunos y meriendas, Alojamiento, Tecnología, Compras, Otros",
+        "description": "Una descripción con un texto devuelto por 
+        el chat-bot explicando por qué el lugar fué incluido en la lista",
+        "address": "Dirección del lugar"
+        "location": { "lat": xx.xxxxx. Debe ser un valor numérico, "lng": xx.xxxxx. Debe ser un valor numérico }
+    },` },
+        { role: 'user', content: userMessage }
+      ],
       model: 'gpt-4-turbo',
     });
 
@@ -17,16 +26,29 @@ async function main() {
 
 export default function Chatbot () {
     const [message, setMessage] = useState()
+    const [responseMessage, setResponseMessage] = useState()
 
-    useEffect(() => {
-        main()
-    },[])
 
+    const handleSend = async () => {
+        const response = await getData(message)
+        setResponseMessage(response)
+    }
 
 
     return (
         <div>
-            <textarea></textarea>
-        </div>
+        <textarea 
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Escribe tu mensaje aquí"
+        />
+        <button onClick={handleSend}>Enviar</button>
+        <textarea 
+            value={responseMessage}
+            readOnly
+            placeholder="Respuesta de la IA"
+        />
+    </div>
+
     )
 }
